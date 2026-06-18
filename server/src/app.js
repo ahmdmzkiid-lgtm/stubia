b282bd66ea22780f05bd348eea3dfa958ee08732
ahmdmzkiid <ahmadmuzaki.ids@gmail.com>
2026-06-18 12:57:28 +0700

    bismillah

--- server/src/app.js
+++ server/src/app.js
@@ -13,14 +13,11 @@ const { populateQuestionHashes } = require('./utils/populateQuestionHashes');
 const app = express();
 
 // Middleware
-app.use(helmet({
-  crossOriginOpenerPolicy: false,
-}));
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production'
-    ? process.env.CLIENT_URL || '*'
-    : '*',
+  origin: ['https://stubia.id', 'https://www.stubia.id', 'http://localhost:5173'],
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 app.use(express.json());
 app.use(morgan('dev'));
